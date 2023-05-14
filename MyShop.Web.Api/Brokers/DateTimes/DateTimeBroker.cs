86eb0e579d7ed88b20cb0e8e2e77d0cd347a7ec4
krishnakumarCBC <mkrishnakumarnaidu@gmail.com>
2023-05-14 13:09:47 +0530

    Added Brokers

---
+++ MyShop.Web.Api/Brokers/DateTimes/DateTimeBroker.cs
@@ -0,0 +1,7 @@
+﻿namespace MyShop.Web.Api.Brokers.DateTimes
+{
+    public class DateTimeBroker : IDateTimeBroker
+    {
+       public DateTimeOffset GetCurrentDateTime() => DateTime.UtcNow;
+    }
+}
---
+++ MyShop.Web.Api/Brokers/DateTimes/IDateTimeBroker.cs
@@ -0,0 +1,7 @@
+﻿namespace MyShop.Web.Api.Brokers.DateTimes
+{
+    public interface IDateTimeBroker
+    {
+        DateTimeOffset GetCurrentDateTime();
+    }
+}
