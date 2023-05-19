31f275b8ad139c9f6312c153e867ef3bd0490a0b
krishnakumarCBC <mkrishnakumarnaidu@gmail.com>
2023-05-19 22:30:20 +0530

    Added Stock

---
+++ MyShop.Web.Api/Models/Stocks/Stock.cs
@@ -0,0 +1,9 @@
+﻿namespace MyShop.Web.Api.Models.Stocks
+{
+    public class Stock : Audit
+    {
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public int CurrentStock { get; set; }
+    }
+}
